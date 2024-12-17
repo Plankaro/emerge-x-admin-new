0cc33e30cca539188d5be1ee8133d3caed4b1957
rabin97 <rabinkarmakar947@gmail.com>
2024-12-18 01:11:49 +0530

    feat:add sidebar color to white

--- src/components/app-layout/app-sidebar.tsx
+++ src/components/app-layout/app-sidebar.tsx
@@ -77,7 +77,7 @@ const data = {
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar collapsible="icon" {...props} className="">
       <SidebarHeader>
         <TeamSwitcher teams={data.teams} />
       </SidebarHeader>
